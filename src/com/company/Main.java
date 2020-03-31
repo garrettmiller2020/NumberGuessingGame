560c75f6760eef1955afba9c0248aae6072fe755
Garrett <garrett.miller.2020@student.lhschools.org>
2020-03-31 15:02:07 -0400

    Rules Printed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    System.out.println("Welcome to the number guessing game.");
+        System.out.println("Here are the rules!");
+        System.out.println("First, I pick a number and then you try to guess it.");
+        System.out.println("The number that I pick will be between 1 and 100.");
+        System.out.println("If you guess lower than my number, I will let you know that you guessed too low,");
+        System.out.println("and if you guess higher than my number, I will let you know that you guessed too high.\n" +
+                "\n");
+        System.out.println("Time To Play!" +
+                "\n");
+
     }
 }
